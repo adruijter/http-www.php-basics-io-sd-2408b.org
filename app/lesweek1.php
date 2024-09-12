f688274e74bd5e25d774151eb34efa36a1ecb233
adruijter <adruijter@gmail.com>
2024-09-12 15:03:40 +0200

    Initial commit

---
+++ app/lesweek1.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="../img/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>PHP Basics</title>
+</head>
+<body>
+    <h1>Lesweek 1</h1>
+
+    <?php
+
+        $voornaam = "Arjan";
+        $leeftijd = 56;
+        $woonplaats = "Alkmaar";
+
+
+
+        echo "<p>Mijn naam is $voornaam</p>";
+        echo "<p>Mijn leeftijd is $leeftijd</p>";
+        echo "<p>Ik woon in $woonplaats</p>";
+    ?>
+
+    <a href="../index.php">home</a>
+</body>
+</html>
+
