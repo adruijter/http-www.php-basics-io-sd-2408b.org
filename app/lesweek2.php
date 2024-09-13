8524852a53161a70c644fe284f3db7dbb3a28115
adruijter <adruijter@gmail.com>
2024-09-13 10:52:44 +0200

    Vermenigvuldiging

--- app/lesweek2.php
+++ app/lesweek2.php
@@ -11,9 +11,28 @@
     <h1>Lesweek 2</h1>
 
     <?php
+        // Hieronder staat een string-variabele
+        $schoolnaam = 'MBO Utrecht';
+        echo "<p>Ik zit op $schoolnaam</p>";
+
+        $aantalStudenten = 489;
+        echo "<p>Het aantal studenten op school is $aantalStudenten</p>";
+
+        $stijgingStudenten = 200;
+        echo "<p>Het nieuwe aantal studenten is " . $aantalStudenten + $stijgingStudenten . " we zijn in aantal gestegen</p>";
+        
+        /**
+         * Hier gebruiken we een nieuwe variabele voor het weergeven van de stijging
+         */
+        $totaalAantalStudenten = $aantalStudenten + $stijgingStudenten;
+        echo "<p>Het nieuwe aantal studenten is $totaalAantalStudenten we zijn in aantal gestegen</p>";
+
+        $verdrievoudiging = 3;
+        $totaalAantalStudenten = $totaalAantalStudenten * $verdrievoudiging;
+        echo "Over 3 jaar verwachten we een verdrievoudiging van het aantal studenten naar $totaalAantalStudenten";
 
 
     ?>
-    
+
 </body>
 </html>
