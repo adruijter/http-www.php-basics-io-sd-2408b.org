c3f7406c62c4756d34712b38308d15ffac264e4d
adruijter <adruijter@gmail.com>
2024-10-04 11:55:50 +0200

    laaatste commit

--- app/lesweek4.php
+++ app/lesweek4.php
@@ -36,7 +36,7 @@
      * Het is bar en boos weer vandaag
      */
 
-    $mooiWeerOfNiet = "Mooi weer";
+    $mooiWeerOfNiet = "Slecht weer";
 
     if ($mooiWeerOfNiet == 'Mooi weer') {
         echo "<p>Het is mooi weer vandaag</p>";
@@ -118,9 +118,10 @@
 
       /**
        * Hieronder een voorbeel van de controlestructuur switch-case
+       * Opdracht: Maak de switch-case af voor de waarden 'avond' en 'nacht'
        */
 
-      $deelVanDeDag = 'middag';
+      $deelVanDeDag = 'banaan';
 
       switch ($deelVanDeDag) {
         case 'ochtend':
@@ -129,8 +130,164 @@
         case 'middag':
             echo "<p>Goedemiddag, het is middag</p>";
             break;
+        case 'avond':
+            echo "<p>Goedenavond, het is avond</p>";
+            break;
+        case 'nacht':
+            echo "<p>Goedennacht, het is nacht</p>";
+            break;
+        default:
+            echo "<p>Het dagdeel is onbekend</p>";            
       }
 
+      /**
+       * Maak een functie diplomaChecker() met daarin 
+       * een switch case controle structuur die aangeeft welke
+       * opleiding je hebt gehaald. De keuze is:
+       * VMBO gemengd, kader, basis, TL, HAVO, VWO
+       * Als de keuze er niet bijzit dan een melding:
+       * Uw diploma is niet bekent 
+       * 
+       * 
+       * Voorbeelden:
+       * $education = "basisschool";
+       * Output
+       * Uw diploma is niet bekend
+       * 
+       * $education = "VMBO TL";
+       * Output:
+       * Ik heb mijn VMBO TL diploma gehaald
+       */
+    //    $education = "";
+
+       if (isset($_POST['submit'])) {
+        $education = $_POST['school'];
+       } else {
+        $education = '';
+       }
+
+       switch ($education) {
+            case 'VMBO TL':
+                echo "<p>Ik heb mijn VMBO TL diploma gehaald</p>";
+                break;
+            case 'VMBO Kader':
+                echo "<p>Ik heb mijn VMBO Kader diploma gehaald</p>";
+                break;
+            case 'VMBO Gemengd':
+                echo "<p>Ik heb mijn VMBO Gemengd diploma gehaald</p>";
+                break;
+            case 'VMBO Basis':
+                echo "<p>Ik heb mijn VMBO Basis diploma gehaald</p>";
+                break;
+            case 'HAVO':
+                echo "<p>Ik heb mijn HAVO diploma gehaald</p>";
+                break;
+            case 'VWO':
+                echo "<p>Ik heb mijn VWO diploma gehaald</p>";
+                break;
+            default:
+                echo "<p>Uw heeft geen schoolkeuze gemaakt, probeer het opnieuw</p>";
+       }
+
+
+       /**
+        * While Loop
+        */
+
+       $number = 1;
+       while ($number <= 10) {
+            echo "<p>$number</p>";
+            $number++;
+       }
+
+       /**
+        * Maak een indexed array groenten met daarin 5 groentensoorten
+        * Gebruik een while loop om de groenten onder elkaar op het scherm
+        * te krijgen.
+        */
+       $groenten = array('Komkommer', 'Sla', 'Tomaat', 'Paprika');
+       echo "Het aantal elementen in het groentenarray is: " . count($groenten);
+
+       $index = 0;
+       while ($index < count($groenten)) {
+            echo "<p>$groenten[$index]</p>";
+            $index++;
+       }
+
+
+       /**
+        * Do while loop (wordt altijd minimaal 1 maal het codeblock uitgevoerd)
+        */
+       $number = 1;
+
+       do {
+        echo "<p>$number</p>";
+        $number++;
+       } while ($number < 5);
+
+
+       $index = 0;
+
+       do {
+        echo "<p>$groenten[$index]</p>";
+        $index++;
+       } while($index < count($groenten));
+
+
+
+       /**
+        * Foreach
+        */
+       foreach ($groenten as $groente) {
+        echo "<p>$groente</p>";
+       }
+
+
+       /**
+        * De for-loop
+        */
+    
+       for ($i = 0; $i < 4; $i++) {
+        echo "<p>$i</p>";
+       }
+
+       $fruit = array('Appel', 'Peer', 'Druif', 'Mandarijn');
+
+       for ($i = 0; $i < count($fruit); $i++) {
+            echo "<p>$fruit[$i]</p>";
+       }
+
+       /**
+        * Schrijf een functie die checked of een fruitsoort in het 
+        * array zit en die deze fruitsoort teruggeeft. Als het er niet in zit
+        * krijg je de melding fruitsoort onbekend.
+        *
+        * fruitChecker('Banaan'); //Output: Banaan
+        * fruitChecker('Kiwi'); //Output: Fruitsoort Kiwi is onbekend
+        *
+        * Gebruik een for-loop en een if-else controlestructuur in de functie 
+        * en de array functie in_array(); https://www.w3schools.com/php/func_array_in_array.asp
+        */ 
+
+
+
+
+
+
+
+
     ?>
+
+
+    <form action="lesweek4.php" method="post">
+        <select name="school" >
+            <!-- Maak meerdere option tages -->
+            <option value="">Maak een schoolkeuze</option>
+            <option value="VMBO TL">VMBO TL</option>
+            <option value="VMBO Kader">VMBO Kader</option>
+            <option value="VMBO Basis">VMBO Basis</option>
+        </select>
+        <input type="submit" name="submit" value="verstuur">
+    </form>
 </body>
 </html>
